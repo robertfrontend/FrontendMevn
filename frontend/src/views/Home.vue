aae79782f1affd036138e61f7029380f18cae933
Robert Romero Medina <enanow41romero@gmail.com>
2021-05-06 20:40:48 -0400

    delete producto

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -60,6 +60,10 @@
               </tr>
             </tbody>
           </table>
+          <div class="spinner-border" role="status" v-if="loading">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <template v-if="loading"> Cargando... </template>
         </div>
       </div>
     </div>
@@ -86,25 +90,82 @@ export default {
           fecha: "20 de agosto 2020",
         },
       ],
+
+      loading: false,
     };
   },
 
   created() {
-    this.axios.get('http://localhost:3000/productos').then((response) => {
-      console.log(response.data);
-    });
+    this.getProductos();
   },
 
   methods: {
     agregarProducto() {
+      this.loading = true;
+
       this.list.push({
         nombre: this.form.nombre,
         precio: this.form.precio,
       });
+
+      let datos = {
+        nombre: this.form.nombre,
+        descripcion: "No se sabe",
+        precio: this.form.precio,
+        fecha: new Date()
+      };
+
+      try {
+        this.axios.post("http://localhost:3000/productos", datos).then((res) => {
+          console.log(res, "todo ok post");
+        });
+      } catch (error) {
+        console.log(error, "error en agregar un producto");
+      }
+      this.loading = false;
+    },
+
+    getProductos() {
+      this.loading = true;
+      this.list = [];
+
+      try {
+        this.axios.get("http://localhost:3000/productos").then((response) => {
+          console.log(response.data);
+          this.list = response.data.body.resultado;
+        });
+      } catch (error) {
+        console.log(error, "Erro en traer los productos");
+      }
+
+      this.loading = false;
     },
 
     selecionarProducto(item) {
+      this.loading = true;
       console.log(item);
+
+      // http://localhost:3000/productos/
+
+      try {
+        this.axios
+          .delete(`http://localhost:3000/productos/${item._id}`)
+          .then((response) => {
+            console.log(response, "Producto eliminado con exito");
+
+            this.getProductos();
+          });
+      } catch (error) {
+        console.log(error, "Error en eliminar el productos");
+      }
+
+      this.loading = false;
+    },
+  },
+
+  watch: {
+    loading() {
+      console.log(this.loading);
     },
   },
 };
